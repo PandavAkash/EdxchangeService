13df8b5c583ecdd9357704f6502ab43d55c35c1c
akashpandav <akashandav92@gmail.com>
2021-04-26 13:48:17 +0530

    Integrated email services

--- ed-exchange/src/main/java/com/edexchange/controller/QuestionAnswerController.java
+++ ed-exchange/src/main/java/com/edexchange/controller/QuestionAnswerController.java
@@ -4,12 +4,15 @@ package com.edexchange.controller;
 import com.edexchange.entities.Answer;
 import com.edexchange.entities.Question;
 import com.edexchange.service.QuestionAnswerService;
+import com.edexchange.service.email.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,6 +22,9 @@ public class QuestionAnswerController {
 
     @Autowired
     private QuestionAnswerService questionAnswerService;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     @PostMapping("/questions/post-que/{userId}")
     public ResponseEntity<String> addNewQuestion(@RequestBody Question question,
@@ -86,13 +92,13 @@ public class QuestionAnswerController {
         }
     }
 
-    @PostMapping("/answers/add-vote/{id}")
+    @GetMapping("/answers/add-vote/{id}")
     public ResponseEntity<String> addVoteToAns(@PathVariable("id")Integer ansId) {
 
         try {
             questionAnswerService.addVoteToAns(ansId);
             return new ResponseEntity<String>(HttpStatus.OK);
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
             return  new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -100,6 +106,10 @@ public class QuestionAnswerController {
     @GetMapping("/answers/get-all")
     public void getAllAns() {
 
+    }
+    @GetMapping("/sendmail/{name}")
+    public void endMail(String name) throws MessagingException, IOException {
+        emailSenderService.sendMail(name);
     }
 }
  
---
+++ ed-exchange/src/main/java/com/edexchange/service/email/EmailSenderService.java
@@ -0,0 +1,77 @@
+package com.edexchange.service.email;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.edexchange.service.email.model.Mail;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+    private static Logger log = LoggerFactory.getLogger(EmailSenderService.class);
+
+
+    public void sendEmail(Mail mail) throws MessagingException, IOException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+//        helper.addAttachment("template-cover.png", new ClassPathResource("javabydeveloper-email.PNG"));
+
+        Context context = new Context();
+        context.setVariables(mail.getProps());
+    
+        String html = templateEngine.process("edexchange-templates", context);
+
+        helper.setTo(mail.getMailTo());
+        helper.setText(html, true);
+        helper.setSubject(mail.getSubject());
+        helper.setFrom(mail.getFrom());
+
+        emailSender.send(message);
+    }
+
+    public void sendMail(String receiverName) throws MessagingException, IOException {
+
+
+        log.info("START... Sending email");
+
+        Mail mail = new Mail();
+        mail.setFrom("");//replace with your desired email
+        mail.setMailTo("");//replace with your desired email
+        mail.setSubject("Appreciation from the EdExchange!!");
+
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("name", "Akash");
+        model.put("location", "Pune");
+        model.put("sign", "EDXCHANGE Team");
+        mail.setProps(model);
+
+        sendEmail(mail);
+        log.info("END... Email sent success");
+
+
+    }
+
+}
